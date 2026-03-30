22ac5d8793dbb5920c354ad346c55a44abd18c25
Felipe De Andrade <felipe.deam98@gmail.com>
2026-03-30 02:15:33 -0300

    feat(pages): add UI to fallback page

--- src/pages/Fallback/index.tsx
+++ src/pages/Fallback/index.tsx
@@ -1,5 +1,35 @@
-const FallbackPage = () => {
-  return <div>FallbackPage</div>;
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { NavLink } from "react-router";
+
+const FallbackPage: React.FC = () => {
+  return (
+    <div className="grid min-h-screen w-full place-items-center px-4 bg-linear-to-r from-blue-300 to-blue-400">
+      <Card className="w-full max-w-md flex">
+        <CardHeader className="text-center">
+          <CardTitle variant={"huge"} className="py-4">
+            Oops!
+          </CardTitle>
+          <CardDescription>
+            The page you are looking for does not exist.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <NavLink to={"/"}>
+            <Button variant="outline" className="w-full">
+              Go Back Home
+            </Button>
+          </NavLink>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default FallbackPage;
