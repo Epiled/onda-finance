6bc80c16f7d60d478884b33698b587490a93dad0
Felipe De Andrade <felipe.deam98@gmail.com>
2026-03-30 09:15:30 -0300

    feat(dashboard): update monthly income and expenses in authStore

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,91 @@
+import { useEffect } from "react";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
+import { useAuthStore } from "@/hooks/useAuthStore";
+
 import data from "../../mocks/dashboardData.json";
 
 const DashboardPage: React.FC = () => {
+  const { setBalance, setIncomeMonth, setExpenseMonth } =
+    useAuthStore();
+
+  useEffect(() => {
+    async function fetchUserBalance() {
+      setTimeout(() => {
+        const balanceBank = 5440.65;
+        // 5440.6500000000015
+
+        setBalance(balanceBank);
+      }, 1000);
+    }
+
+    fetchUserBalance();
+  }, [setBalance]);
+
+  useEffect(() => {
+    const now = new Date();
+    const currentMonth = now.getMonth();
+    const currentYear = now.getFullYear();
+
+    const totals = data.reduce(
+      (acc: { income: 0; expense: 0 }, item) => {
+        const itemDate = new Date(item.date);
+        const itemMonth = itemDate.getMonth();
+        const itemYear = itemDate.getFullYear();
+
+        if (itemMonth !== currentMonth || itemYear !== currentYear) {
+          return acc;
+        }
+
+        if (
+          item.type === "INCOME" &&
+          item.status !== "FAILED" &&
+          item.status !== "PENDING"
+        ) {
+          acc.income += item.value;
+        }
+
+        if (item.type === "EXPENSE" && item.status !== "FAILED") {
+          acc.expense += item.value;
+        }
+
+        return acc;
+      },
+      { income: 0, expense: 0 },
+    );
+
+    setIncomeMonth(totals.income);
+    setExpenseMonth(totals.expense);
+  }, [setExpenseMonth, setIncomeMonth]);
+
+  useEffect(() => {
+    const totals = data.reduce(
+      (acc: { income: 0; expense: 0 }, item) => {
+        if (
+          item.type === "INCOME" &&
+          item.status !== "FAILED" &&
+          item.status !== "PENDING"
+        ) {
+          acc.income += item.value;
+        }
+
+        if (item.type === "EXPENSE" && item.status !== "FAILED") {
+          acc.expense += item.value;
+        }
+
+        return acc;
+      },
+      { income: 0, expense: 0 },
+    );
+
+    console.log(totals.income - totals.expense);
+  }, []);
+
   return (
     <SidebarProvider
       style={
