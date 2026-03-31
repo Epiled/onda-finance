b83900a9061f19d05d3773725c7fd2c6e00857ae
Felipe De Andrade <felipe.deam98@gmail.com>
2026-03-31 14:56:07 -0300

    refactor(Dashboard): replace api logic to use service

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 
 import { AppSidebar } from "@/components/app-sidebar";
 import { DataTable } from "@/components/data-table";
@@ -6,64 +6,34 @@ import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
+import { useUser } from "@/hooks/useUser";
+import { useTransactions } from "@/hooks/useTransactions";
 import { useAuthStore } from "@/hooks/useAuthStore";
-import { useTransactionStore } from "@/hooks/useTransactionStore";
+
+import { TransactionService } from "@/services/transactions.service";
 
 const DashboardPage: React.FC = () => {
-  const { user, setBalance, setIncomeMonth, setExpenseMonth } = useAuthStore();
-  const { transactions } = useTransactionStore();
+  TransactionService.getAll();
+  const user = useUser();
 
-  const userTransactions = useMemo(() => {
-    return transactions.filter((item) => item.foreignKey === user?.id);
-  }, [transactions, user?.id]);
+  const { data, isLoading } = useTransactions(user.id);
 
-  const balance = useMemo(() => {
-    return userTransactions.reduce((acc, tx) => {
-      if (tx.status !== "COMPLETED") return acc;
-
-      return tx.type === "INCOME" ? acc + tx.value : acc - tx.value;
-    }, 0);
-  }, [userTransactions]);
+  const { setBalance, setIncomeMonth, setExpenseMonth } = useAuthStore();
 
   useEffect(() => {
-    setBalance(balance);
-  }, [balance, setBalance]);
+    setBalance(data?.balance ?? 0);
+    setIncomeMonth(data?.totals.income ?? 0);
+    setExpenseMonth(data?.totals.expense ?? 0);
+  }, [
+    data?.balance,
+    data?.totals.expense,
+    data?.totals.income,
+    setBalance,
+    setExpenseMonth,
+    setIncomeMonth,
+  ]);
 
-  useEffect(() => {
-    const now = new Date();
-    const currentMonth = now.getMonth();
-    const currentYear = now.getFullYear();
-
-    const totals = userTransactions.reduce(
-      (acc: { income: 0; expense: 0 }, item) => {
-        const itemDate = new Date(item.date);
-        const itemMonth = itemDate.getMonth();
-        const itemYear = itemDate.getFullYear();
-
-        if (itemMonth !== currentMonth || itemYear !== currentYear) {
-          return acc;
-        }
-
-        if (
-          item.type === "INCOME" &&
-          item.status !== "FAILED" &&
-          item.status !== "PENDING"
-        ) {
-          acc.income += item.value;
-        }
-
-        if (item.type === "EXPENSE" && item.status !== "FAILED") {
-          acc.expense += item.value;
-        }
-
-        return acc;
-      },
-      { income: 0, expense: 0 },
-    );
-
-    setIncomeMonth(totals.income);
-    setExpenseMonth(totals.expense);
-  }, [setExpenseMonth, setIncomeMonth, userTransactions]);
+  const transactionsList = data?.transactions ?? [];
 
   return (
     <SidebarProvider
@@ -81,7 +51,7 @@ const DashboardPage: React.FC = () => {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
-              <DataTable data={userTransactions} />
+              <DataTable data={transactionsList} />
             </div>
           </div>
         </div>
