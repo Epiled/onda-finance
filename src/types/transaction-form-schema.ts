c04b33c98f606afe76e2fc62cf2949ec9a14fd57
Felipe De Andrade <felipe.deam98@gmail.com>
2026-03-31 14:44:35 -0300

    chore: move transaction-form-schema to types directory

--- src/types/transaction-form-schema.ts
+++ src/types/transaction-form-schema.ts
@@ -1,9 +1,9 @@
 import * as z from "zod";
 
-export const transactionSchema = z.object({
+export const transactionSchemaForm = z.object({
   from: z.email("E-mail do remetente inválido"),
   to: z.email("Informe um e-mail de destino válido"),
   value: z.number("O valor deve ser um número").min(0.01, "O valor deve ser maior que zero"),
 });
 
-export type TransactionFormValues = z.infer<typeof transactionSchema>;
+export type TransactionFormValues = z.infer<typeof transactionSchemaForm>;
